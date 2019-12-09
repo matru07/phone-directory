9da979525fe225d0401367e197b84b003e00f768
Chaitanya Mahajan <chatumahajan07@gmail.com>
2019-12-10 00:32:08 +0530

    Routing: Added routing for Add Subscriber page and Show Subscribers page

--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header.js';
 import './ShowSubscribers.css';
+import { Link } from 'react-router-dom';
 
 class ShowSubscribers extends Component {
 
@@ -11,7 +12,11 @@ class ShowSubscribers extends Component {
       <div>
         <Header heading="Phone Directory" />
         <div className="component-body-container">
-          <button className="custom-btn add-btn">Add</button>
+        <Link to="/add">
+            <button className="custom-btn add-btn">Add</button>
+          </Link>
+
+
           <div className="grid-container heading-container">
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
